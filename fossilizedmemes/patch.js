74631d324dbea0abcf62e6073f1e340049939f4c
aimkoane <renkusa691@gmail.com>
2025-10-28 13:53:54 -0400

    Initial Commit

---
+++ fossilizedmemes/patch.js
@@ -0,0 +1,13 @@
+{
+    "id": "fossilizedmemes",
+    "servers": [
+        "https://mirrors.thpatch.net/aimkoane/fossilizedmemes/"
+    ],
+    "title": "Touhou Kinjoukyou ~ Fossilized Memes",
+    "dependencies": [
+        "nmlgc/base_tsa",
+        "nmlgc/script_latin",
+        "nmlgc/western_name_order"
+        "thpatch/lang_en"
+    ]
+}
---
+++ repo.js
@@ -0,0 +1,11 @@
+{
+    "contact": "discord @aimkoane",
+    "id": "aimkoane",
+    "patches": {
+        "fossilizedmemes": "Touhou 20 but memes",
+    },
+    "servers": [
+        "https://mirrors.thpatch.net/aimkoane/"
+    ],
+    "title": "aimkoane's dumb ideas"
+}
