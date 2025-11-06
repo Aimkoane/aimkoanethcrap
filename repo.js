90fd33915e754d2d5e87f0c2ced3700d90b2883f
Aimkoane <renkusa691@gmail.com>
2025-11-06 08:37:47 -0500

    Update TouhouMemeProject description
    Expanded description of the TouhouMemeProject.

--- repo.js
+++ repo.js
@@ -2,7 +2,7 @@
     "contact": "discord @aimkoane",
     "id": "aimkoane",
     "patches": {
-        "TouhouMemeProject": "Touhou but memes",
+        "TouhouMemeProject": "Touhou but memes. So far, almost fully modded 20, started work on 14, 15, 17, and 18.",
     },
     "servers": [
         "https://github.com/Aimkoane/aimkoanethcrap"
